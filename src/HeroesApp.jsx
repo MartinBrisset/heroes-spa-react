2b8afcb0fc7be9fc504bcd641b8d7a86d322c085
martin <brissetmartin@gmail.com>
2022-10-10 16:52:24 -0300

    Navbar

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -3,7 +3,6 @@ import { AppRouter } from "./router/AppRouter"
 export const HeroesApp = () => {
   return (
     <>
-        <h1>HeroesApp</h1>
         <AppRouter />
     </>
   )
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,10 +2,13 @@ import { Navigate, Route, Routes } from "react-router-dom"
 import { MarvelPage } from "../heroes/pages/MarvelPage";
 import { DCPage } from "../heroes/pages/DCPage";
 import { LoginPage } from "../auth/pages/LoginPage";
+import { Navbar } from "../ui";
 
 export const AppRouter = () => {
   return (
     <>
+        <Navbar />
+
         <Routes>
             <Route path='marvel' element={<MarvelPage />} />
             <Route path='dc' element={<DCPage />} />
